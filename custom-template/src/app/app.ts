90fcbc69eb25612664f8fe0810872ee37cf6ff61
Nandhakumar <nandhu_kumar86@yahoo.com>
2025-06-28 07:12:00 +0530

    a4 page initial code

--- custom-template/src/app/app.ts
+++ custom-template/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { A4PageComponent} from './a4-page/a4-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [A4PageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true
